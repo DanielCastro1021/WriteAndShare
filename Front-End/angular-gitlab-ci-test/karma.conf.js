f983e6600a46227690903db8e9781a5e1c10e200
Daniel Castro <danielcastrofafe@gmail.com>
2020-02-12 17:41:15 +0000

    CI Files Updated

--- Front-End/angular-gitlab-ci-test/karma.conf.js
+++ Front-End/angular-gitlab-ci-test/karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require("path").join(__dirname, "./coverage/angular-gitlab-ci-test"),
+      dir: require("path").join(__dirname, "../coverage"),
       reports: ["html", "lcovonly", "text-summary"],
       fixWebpackSourcePaths: true
     },
@@ -29,12 +29,11 @@ module.exports = function(config) {
     autoWatch: true,
     browsers: ["Chrome"],
     customLaunchers: {
-      ChromeHeadlessCI: {
+      ChromeHeadlessNoSandbox: {
         base: "ChromeHeadless",
         flags: ["--no-sandbox"]
       }
     },
-    singleRun: false,
-    restartOnFileChange: true
+    singleRun: false
   });
 };
