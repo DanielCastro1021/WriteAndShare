516833a1052d8e981cbfae16a0098cf5419e298b
Daniel Castro <danielcastrofafe@gmail.com>
2020-02-14 14:19:59 +0000

    Adicionada componente do Sistema de Autenticação

--- angular-write-and-share/src/app/app.module.ts
+++ angular-write-and-share/src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AuthenticationComponent } from "./components/authentication/authentication.component";
+import { MainPageComponent } from "./components/web/main-page/main-page.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, AuthenticationComponent, MainPageComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
