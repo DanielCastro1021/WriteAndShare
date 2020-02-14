798bb46773ae0e34e7e361b12a451ed664ee094d
Daniel Castro <danielcastrofafe@gmail.com>
2020-02-14 14:23:23 +0000

    Atualizado Controller da componente de Sistema de Autenticação

--- angular-write-and-share/src/app/components/authentication/authentication.component.ts
+++ angular-write-and-share/src/app/components/authentication/authentication.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
+import { User } from "src/app/models/User";
+import { AuthenticationService } from "src/app/services/authentication/authentication.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-authentication",
@@ -6,7 +9,45 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./authentication.component.css"]
 })
 export class AuthenticationComponent implements OnInit {
-  constructor() {}
+  @Input() userData: User = new User();
 
-  ngOnInit(): void {}
+  constructor(
+    public authencticationService: AuthenticationService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {}
+
+  /**
+   * This function authenticates the credentials of a user.
+   */
+  login() {
+    console.log(this.userData);
+    if (this.userData.username === "") {
+      alert("Please insert username.");
+    } else if (this.userData.password === "") {
+      alert("Please insert password.");
+    } else {
+      this.serviceLogin();
+    }
+  }
+
+  /**
+   * This function authenticates the credentials of a user, in the the REST API.
+   */
+  serviceLogin(): void {
+    this.authencticationService
+      .login(this.userData.username, this.userData.password)
+      .subscribe(
+        result => {
+          this.router.navigate(["/campaigns"]);
+        },
+        err => {
+          console.log(err);
+          alert(
+            "Sorry we could not found you, please check if username and password are correct."
+          );
+        }
+      );
+  }
 }
