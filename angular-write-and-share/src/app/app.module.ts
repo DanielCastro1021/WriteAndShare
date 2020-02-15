cef426fdd127f5d151d4fba6c64bf849d16138e0
Daniel Castro <danielcastrofafe@gmail.com>
2020-02-15 17:18:27 +0000

    Adicionada Componente relativa à Lista de Amigos

--- angular-write-and-share/src/app/app.module.ts
+++ angular-write-and-share/src/app/app.module.ts
@@ -1,20 +1,63 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MainPageComponent } from "./components/website/main-page/main-page.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FriendsListComponent } from "./components/friends/friends-list/friends-list.component";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MainPageComponent } from './components/website/main-page/main-page.component';
+import { A11yModule } from "@angular/cdk/a11y";
+import { BidiModule } from "@angular/cdk/bidi";
+import { ObserversModule } from "@angular/cdk/observers";
+import { OverlayModule } from "@angular/cdk/overlay";
+import { PlatformModule } from "@angular/cdk/platform";
+import { PortalModule } from "@angular/cdk/portal";
+import { CdkStepperModule } from "@angular/cdk/stepper";
+import { CdkTableModule } from "@angular/cdk/table";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { MatButtonModule } from "@angular/material/button";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatChipsModule } from "@angular/material/chips";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatDialogModule } from "@angular/material/dialog";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainPageComponent
-  ],
+  declarations: [AppComponent, MainPageComponent, FriendsListComponent],
   imports: [
+    A11yModule,
+    BidiModule,
+    ObserversModule,
+    OverlayModule,
+    PlatformModule,
+    PortalModule,
+    CdkStepperModule,
+    CdkTableModule,
+    FlexLayoutModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatGridListModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
