3d449932db521a2a9d0857d8c7f1d0013d6c0e90
Daniel Castro <danielcastrofafe@gmail.com>
2020-02-15 12:18:39 +0000

    Correção do nome da Componente

--- angular-write-and-share/src/app/components/registration/registration.component.spec.ts
+++ angular-write-and-share/src/app/components/registration/registration.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ResgistrationComponent } from './resgistration.component';
+import { RegistrationComponent } from './registration.component';
 
-describe('ResgistrationComponent', () => {
-  let component: ResgistrationComponent;
-  let fixture: ComponentFixture<ResgistrationComponent>;
+describe('RegistrationComponent', () => {
+  let component: RegistrationComponent;
+  let fixture: ComponentFixture<RegistrationComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ResgistrationComponent ]
+      declarations: [ RegistrationComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ResgistrationComponent);
+    fixture = TestBed.createComponent(RegistrationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ angular-write-and-share/src/app/components/registration/registration.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { RegistrationService } from "src/app/services/registration/registration.service";
+import { Router } from "@angular/router";
+import { User } from "src/app/models/User";
+import { ValidationService } from "src/app/services/validation/validation.service";
+
+@Component({
+  selector: "app-registration",
+  templateUrl: "./registration.component.html",
+  styleUrls: ["./registration.component.css"]
+})
+export class RegistrationComponent implements OnInit {
+  @Input() userData: User = new User();
+  cpassword: string;
+
+  constructor(
+    private resgistrationService: RegistrationService,
+    private validationService: ValidationService,
+    private router: Router
+  ) {}
+  ngOnInit(): void {}
+
+  /**
+   * This function registers the user, if form is valid.
+   */
+  register(): void {
+    console.log(this.userData);
+    if (
+      this.validationService.validateUsername(this.userData.username) &&
+      this.validationService.validatePassword(
+        this.userData.password,
+        this.cpassword
+      )
+    ) {
+      this.registerUserService();
+    }
+  }
+
+  /**
+   * This function posts the user, in the REST API.
+   */
+  registerUserService(): void {
+    this.resgistrationService.register(this.userData).subscribe(
+      result => {
+        console.log(result);
+        this.router.navigate(["/login"]);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  swithToSignInPage() {
+    this.router.navigate(["/sign_in"]);
+  }
+}
