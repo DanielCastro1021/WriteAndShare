c5514321422b7da0a0d3dbe74a8485f2f018df4b
Daniel Castro <danielcastrofafe@gmail.com>
2020-02-15 11:27:39 +0000

    Adicionada a Componente relativa ao Sistema de Registo

--- angular-write-and-share/src/app/app.module.ts
+++ angular-write-and-share/src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MainPageComponent } from './components/website/main-page/main-page.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MainPageComponent } from "./components/website/main-page/main-page.component";
+import { ResgistrationComponent } from "./components/resgistration/resgistration/resgistration.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainPageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MainPageComponent, ResgistrationComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
