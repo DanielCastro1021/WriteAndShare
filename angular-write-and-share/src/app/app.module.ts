3ff24033e27104280d38ab2e5c478df841ab43b1
Daniel Castro <danielcastrofafe@gmail.com>
2020-02-15 15:45:45 +0000

    Atualização do HTML and CSS  da Componente

--- angular-write-and-share/src/app/app.module.ts
+++ angular-write-and-share/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
-
+import { MatCardModule } from "@angular/material/card";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { AuthenticationComponent } from "./components/authentication/authentication.component";
 import { MainPageComponent } from "./components/website/main-page/main-page.component";
 import { JwtInterceptor } from "./helper/jwt.interceptor";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [AppComponent, MainPageComponent, AuthenticationComponent],
@@ -17,7 +18,9 @@ import { JwtInterceptor } from "./helper/jwt.interceptor";
     ReactiveFormsModule,
     FormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
