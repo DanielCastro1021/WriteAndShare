0ba1f55026ac242a8c2f873fa7ac340e1175a76f
Daniel Castro <danielcastrofafe@gmail.com>
2020-02-15 15:59:18 +0000

    Atualização do HTML and CSS da Componente

--- angular-write-and-share/src/app/app.module.ts
+++ angular-write-and-share/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
+import { MatCardModule } from "@angular/material/card";
 import {
   HttpClientModule,
   HTTP_INTERCEPTORS,
@@ -12,10 +13,18 @@ import { JwtInterceptor } from "./helper/jwt.interceptor";
 import { AppComponent } from "./app.component";
 import { MainPageComponent } from "./components/website/main-page/main-page.component";
 import { RegistrationComponent } from "./components/registration/registration.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, MainPageComponent, RegistrationComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    MatCardModule,
+    BrowserAnimationsModule
+  ],
   providers: [
     HttpClient,
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
