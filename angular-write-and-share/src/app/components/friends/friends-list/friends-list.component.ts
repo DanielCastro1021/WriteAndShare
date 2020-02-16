9995f158571d8faed79b6804bd72c7d09af16cac
Daniel Castro <danielcastrofafe@gmail.com>
2020-02-16 17:00:53 +0000

    Funcionalidade de Visualizar Lista de Amigos e Remover Amigos

--- angular-write-and-share/src/app/components/friends/friends-list/friends-list.component.ts
+++ angular-write-and-share/src/app/components/friends/friends-list/friends-list.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { User } from "src/app/models/User";
+import { FriendsService } from "src/app/services/friends/friends.service";
 
 @Component({
-  selector: 'app-friends-list',
-  templateUrl: './friends-list.component.html',
-  styleUrls: ['./friends-list.component.css']
+  selector: "app-friends-list",
+  templateUrl: "./friends-list.component.html",
+  styleUrls: ["./friends-list.component.css"]
 })
 export class FriendsListComponent implements OnInit {
+  friendsList: User[];
 
-  constructor() { }
+  constructor(private friendsService: FriendsService) {}
 
   ngOnInit(): void {
+    this.getFriendsList();
   }
 
+  public getFriendsList(): void {
+    this.friendsService
+      .getFriends(this.friendsService.decodePayloadJWT().unique_name)
+      .subscribe(
+        (friendsList: User[]) => (this.friendsList = friendsList),
+        err => {
+          console.log(err);
+        }
+      );
+  }
+
+  public removeFriend(_id: string): void {
+    this.friendsService
+      .removeFriend(this.friendsService.decodePayloadJWT().unique_name, _id)
+      .subscribe(
+        result => {
+          console.log(result);
+        },
+        err => {
+          console.log(err);
+        }
+      );
+  }
 }
